3f2292ed4ef4a657019ae09b701e5998c4330784
ThisisFinee <108457032+ThisisFinee@users.noreply.github.com>
2023-05-25 21:32:11 +0300

    Some Troubles

--- src/Interface.java
+++ src/Interface.java
@@ -1,18 +1,17 @@
+import javafx.concurrent.Service;
+import javafx.concurrent.Task;
 import java.net.URL;
+import java.util.ArrayList;
+import java.util.LinkedHashSet;
 import java.util.ResourceBundle;
+import java.util.Set;
+
 import javafx.fxml.FXML;
 import javafx.scene.control.Button;
 import javafx.scene.control.TextField;
 import javafx.scene.text.Text;
-import javafx.concurrent.Task;
-import javafx.concurrent.Service;
+
 public class Interface {
-
-    @FXML
-    private ResourceBundle resources;
-
-    @FXML
-    private URL location;
 
     @FXML
     private TextField FloorLabel;
@@ -25,6 +24,12 @@ public class Interface {
 
     @FXML
     private Button RefreshButton;
+
+    @FXML
+    private Button StartButton1;
+
+    @FXML
+    private Button StartButton2;
 
     @FXML
     private Button addFloorButton;
@@ -42,17 +47,32 @@ public class Interface {
     private Text textCondition;
 
     @FXML
+    private Text textCondition1;
+
+    @FXML
     private Text textDirection;
+
+    @FXML
+    private Text textDirection1;
 
     @FXML
     private Text textFloor;
 
     @FXML
+    private Text textFloor1;
+
+    @FXML
     private Text textNextFloor;
+
+    @FXML
+    private Text textNextFloor1;
 
     @FXML
     private TextField timeGenerateLabel;
     Lift lift = new Lift();
+    Lift lift1 = new Lift();
+    int home_floors;
+    ArrayList<Integer> Main_flow = new ArrayList<Integer>();
     @FXML
     void initialize() {
         homeCreateButton.setOnAction(actionEvent -> {
@@ -70,7 +90,34 @@ public class Interface {
             };
             service.start();
         });
-        addFloorButton.setOnAction(actionEvent -> {Service<Void> service = new Service<Void>() {
+
+        StartButton1.setOnAction(actionEvent -> {Service<Void> service1 = new Service<Void>() {
+            @Override
+            protected Task<Void> createTask() {
+                return new Task<Void>() {
+                    @Override
+                    protected Void call() throws Exception {
+                        start_first_lift();
+                        return null;
+                    }
+                };
+            }
+        };
+            service1.start();});
+        StartButton2.setOnAction(actionEvent -> {Service<Void> service2 = new Service<Void>() {
+            @Override
+            protected Task<Void> createTask() {
+                return new Task<Void>() {
+                    @Override
+                    protected Void call() throws Exception {
+                        start_second_lift();
+                        return null;
+                    }
+                };
+            }
+        };
+            service2.start();});
+        addFloorButton.setOnAction(actionEvent -> {Service<Void> service3 = new Service<Void>() {
             @Override
             protected Task<Void> createTask() {
                 return new Task<Void>() {
@@ -82,8 +129,8 @@ public class Interface {
                 };
             }
         };
-            service.start();});
-        GeneratorButton.setOnAction(actionEvent -> {Service<Void> service = new Service<Void>() {
+            service3.start();});
+        GeneratorButton.setOnAction(actionEvent -> {Service<Void> service4 = new Service<Void>() {
             @Override
             protected Task<Void> createTask() {
                 return new Task<Void>() {
@@ -95,8 +142,8 @@ public class Interface {
                 };
             }
         };
-            service.start();});
-        RefreshButton.setOnAction(actionEvent -> {Service<Void> service = new Service<Void>() {
+            service4.start();});
+        RefreshButton.setOnAction(actionEvent -> {Service<Void> service5 = new Service<Void>() {
             @Override
             protected Task<Void> createTask() {
                 return new Task<Void>() {
@@ -108,8 +155,8 @@ public class Interface {
                 };
             }
         };
-            service.start();});
-        FullPuthButton.setOnAction(actionEvent -> {Service<Void> service = new Service<Void>() {
+            service5.start();});
+        FullPuthButton.setOnAction(actionEvent -> {Service<Void> service6 = new Service<Void>() {
             @Override
             protected Task<Void> createTask() {
                 return new Task<Void>() {
@@ -121,14 +168,19 @@ public class Interface {
                 };
             }
         };
-            service.start();});
+            service6.start();});
     }
 
     void Create_button(){
         if (numFloorsLabel.getText().isEmpty()) System.out.println("Не указано количество этажей");
-        else {lift.home_floors = Integer.parseInt(numFloorsLabel.getText());
-        lift.start();
-        lift.next_floor();}
+        else {this.home_floors = Integer.parseInt(numFloorsLabel.getText());
+        }
+    }
+    void start_first_lift(){
+        lift.next_floor();
+    }
+    void start_second_lift(){
+        lift1.next_floor();
     }
     void Add_button(){
         if (FloorLabel.getText().isEmpty()) System.out.println("Не указан этаж");
@@ -136,25 +188,87 @@ public class Interface {
     }
 
     void Generate_button(){
-        if (numFloorsLabel.getText().isEmpty()) System.out.println("Не указано количество добавлений в поток");
+        if (numGenerateLabel.getText().isEmpty()) System.out.println("Не указано количество добавлений в поток");
         else {
-            if (timeGenerateLabel.getText().isEmpty()) System.out.println("Не указан интервал добавления в поток");
-            else lift.Generate_stream(Integer.parseInt(numFloorsLabel.getText()), Integer.parseInt(timeGenerateLabel.getText()));
+            if (timeGenerateLabel.getText().isEmpty()) System.out.println(  "Не указан интервал добавления в поток");
+            else {
+                Generate_stream(Integer.parseInt(numGenerateLabel.getText()), Integer.parseInt(timeGenerateLabel.getText()));}
+        }
+
+    }
+    void Add_main_flow(int fl){
+        if (fl>0 && fl < this.home_floors){
+            this.Main_flow.add(fl);
+        }
+    }
+    void Distribute_lift() {
+        for (int i = 0; i < Main_flow.size();i++){
+            int fl = Main_flow.get(i);
+            System.out.println(fl);
+            System.out.println(this.lift.floor_distance(fl));
+            System.out.println(this.lift1.floor_distance(fl));
+            if (this.lift.floor_distance(fl) <= this.lift1.floor_distance(fl) && !this.lift1.up_floors.contains(fl) && !this.lift1.down_floors.contains(fl)){
+                this.lift.add_floor(fl);
+
+            }
+            else if (this.lift.floor_distance(fl) > this.lift1.floor_distance(fl) && !this.lift.up_floors.contains(fl) && !this.lift.down_floors.contains(fl)){
+                this.lift1.add_floor(fl);
+            }
+        }
+    }
+    void No_Duplicate(){
+        Set<Integer> set = new LinkedHashSet<>(this.Main_flow);
+        ArrayList<Integer> flow = new ArrayList<>(set);
+        this.Main_flow = flow;
+    }
+    void Generate_stream(int numFloors, int timeGenerate){
+        int interval = timeGenerate*1000;
+        for (;;){
+            for (int i = 0; i<numFloors;i++){
+                int fl = (int) (Math.random()*this.home_floors+1);
+                Add_main_flow(fl);
+            }
+            System.out.println(this.Main_flow);
+            No_Duplicate();
+            Distribute_lift();
+            Main_flow.clear();
+            System.out.println(lift.up_floors);
+            System.out.println(lift.down_floors);
+            System.out.println(lift1.up_floors);
+            System.out.println(lift1.down_floors);
+            try {
+                Thread.sleep(interval);
+            } catch (InterruptedException ex) {
+                ex.printStackTrace();
+            }
         }
     }
     void Refresh_Button(){
+        for (;;){
+            try {
+                Thread.sleep(2000);
+            } catch (InterruptedException ex) {
+                ex.printStackTrace();
+            }
         lift.next_num_floor();
         textCondition.setText(lift.condition);
+        textCondition1.setText(lift1.condition);
         textFloor.setText(String.valueOf(lift.floor));
+        textFloor1.setText(String.valueOf(lift1.floor));
         textNextFloor.setText(String.valueOf(lift.next_n_floor));
+        textNextFloor1.setText(String.valueOf(lift1.next_n_floor));
+        System.out.println(lift1.next_n_floor);
         if (lift.direction == 1){textDirection.setText("Вверх");}
         else if (lift.direction == 0){textDirection.setText("Вниз");}
         else {textDirection.setText("Ожидание");}
+        if (lift1.direction == 1){textDirection1.setText("Вверх");}
+        else if (lift1.direction == 0){textDirection1.setText("Вниз");}
+        else {textDirection1.setText("Ожидание");}}
     }
     void Path_button(){
         for (int i = 0; i < lift.full_path.size();i++){
             System.out.print(lift.full_path.get(i));
-            System.out.print("1");
+            System.out.print(" ");
         }
     }
 
--- src/Lift.java
+++ src/Lift.java
@@ -1,19 +1,17 @@
 import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.Collections;
-import java.util.Random;
+
 public class Lift extends Thread {
-    int home_floors;
     int floor;
     ArrayList<Integer> up_floors;
     ArrayList<Integer> down_floors;
     int direction;
     String condition;
     ArrayList<Integer> full_path;
-
     int next_n_floor;
 
 Lift(){
-    home_floors = 20;
     floor = 1;
     up_floors = new ArrayList<>();
     down_floors = new ArrayList<>();
@@ -25,49 +23,64 @@ Lift(){
 
 void add_floor(int fl){
     System.out.println("add");
-    if (fl > this.floor || fl<this.home_floors){
+    if (fl > this.floor && !this.up_floors.contains(fl)){
         this.up_floors.add(fl);
+        Collections.sort(this.up_floors);
         }
-    else if (fl < this.floor || fl>=1){
+    else if (fl < this.floor && !this.down_floors.contains(fl)){
         this.down_floors.add(fl);
+        Collections.sort(this.down_floors, Collections.reverseOrder());
         }
-    else if (fl == this.floor || 1<=fl || fl < this.home_floors){
-        stop_lift(fl);
-    }
+
     }
 void stop_lift(int fl){
     System.out.println("stop");
     this.condition = "Ожидание";
+    while (this.up_floors.isEmpty() && this.down_floors.isEmpty()){
+        ;
+    }
+    if (!this.up_floors.isEmpty()){ this.direction = 1;}
+    else {this.direction = 0;}
     try {
         Thread.sleep(3000);
     } catch (InterruptedException ex) {
         ex.printStackTrace();
     }
-    if (this.direction == 1){
-        this.up_floors.removeIf(n ->(n == fl));
-        this.floor = fl;
+    if (this.direction == 1 && !this.up_floors.isEmpty()){
+        this.up_floors.removeIf(n -> n == fl);
+        this.floor = this.up_floors.get(0);
         if (this.up_floors.isEmpty()){
+            change_direction();
             this.direction = 0;
             next_floor();
         }
     }
-    else if (this.direction == 0){
-        this.down_floors.removeIf(n ->(n == fl));
-        this.floor = fl;
+    else if (this.direction == 1 && this.up_floors.isEmpty() && !this.down_floors.isEmpty()){
+        change_direction();
+        this.direction = 0;
+        next_floor();
+    }
+    else if (this.direction == 0 && !this.up_floors.isEmpty() && this.down_floors.isEmpty()){
+        change_direction();
+        this.direction = 0;
+        next_floor();
+    }
+    else if (this.direction == 0 && !this.down_floors.isEmpty()){
+        this.down_floors.removeIf(n -> n == fl);
+        this.floor = this.down_floors.get(0);
         if (this.down_floors.isEmpty()){
+            change_direction();
             this.direction = 1;
             next_floor();
-        }
+        }}
     else {
-        if (this.up_floors.isEmpty() || this.down_floors.isEmpty()){
-            stop_lift(this.floor);
-        }
-        else {
-            this.direction = 1;
-        }
-        }
+        this.up_floors.removeIf(n -> n == fl);
+        this.floor = this.up_floors.get(0);
+        next_floor();
     }
-}
+
+    }
+
 
 void change_direction(){
     System.out.println("change");
@@ -81,59 +94,104 @@ void change_direction(){
 void next_floor(){
     System.out.println("next");
     this.condition = "Движение";
-    try {
-        Thread.sleep(3000);
-    } catch (InterruptedException ex) {
-        ex.printStackTrace();
-    }
-    if (this.up_floors.isEmpty() || this.down_floors.isEmpty()){
+    if (this.up_floors.isEmpty() && this.down_floors.isEmpty()){
         this.direction = -1;
+        stop_lift(this.floor);
     }
-    else if (this.direction == 1 || this.up_floors.isEmpty()){
+    else if (this.direction == 1 && this.up_floors.isEmpty()){
         change_direction();
         this.direction = 0;
     }
-    else if (this.direction == 0 || this.down_floors.isEmpty()){
+    else if (this.direction == 0 && this.down_floors.isEmpty()){
         change_direction();
         this.direction = 1;
     }
-    if (this.direction == 1){
+    if (this.direction == 1 && !this.up_floors.isEmpty()){
         this.full_path.add(this.floor);
-        this.floor = this.up_floors.get(0);
-        this.up_floors.remove(0);
+        try {
+            Thread.sleep(3000);
+        } catch (InterruptedException ex) {
+            ex.printStackTrace();
+        }
         stop_lift(this.floor);
     }
-    else if (this.direction == 0){
+    else if (this.direction == 0 && !this.down_floors.isEmpty()){
         this.full_path.add(this.floor);
-        this.floor = this.down_floors.get(0);
-        this.down_floors.remove(0);
+        try {
+            Thread.sleep(3000);
+        } catch (InterruptedException ex) {
+            ex.printStackTrace();
+        }
         stop_lift(this.floor);
     }
     else {
         stop_lift(this.floor);
     }
 }
-void Generate_stream(int numb, int interval){
-    System.out.println("Generate");
-    for (int i = 0; i<numb; i++) {
-        int inter = interval * 1000;
-        try {
-            Thread.sleep(inter);
-        } catch (InterruptedException ex) {
-            ex.printStackTrace();
-        }
-        int fl = (int) (Math.random()*this.home_floors+1);
-        add_floor(fl);
-    }
-}
 void next_num_floor(){
     System.out.println("next num");
-    if (this.direction == 1 && this.up_floors != null){
-        this.next_n_floor = this.up_floors.get(0);
+    if (this.direction == 1 && !this.up_floors.isEmpty()){
+        this.next_n_floor = this.up_floors.get(1);
     }
-    else if (this.direction == 0 && this.down_floors != null){
-        this.next_n_floor = this.down_floors.get(0);
+    else if (this.direction == 1 && this.up_floors.isEmpty() && !this.down_floors.isEmpty()){
+        change_direction();
+        this.next_n_floor = this.down_floors.get(1);
+    }
+    else if (this.direction == 0 && this.down_floors.isEmpty() && !this.up_floors.isEmpty()){
+        change_direction();
+        this.next_n_floor = this.up_floors.get(1);
+    }
+    else if (this.direction == 0 && !this.down_floors.isEmpty()){
+        this.next_n_floor = this.down_floors.get(1);
     }
     else this.next_n_floor = this.floor;
 }
+Integer up(int fl, int flag){
+    ArrayList<Integer> up_list = new ArrayList<>(this.up_floors);
+    up_list.add(fl);
+    int index;
+    if (flag == 1) {
+        index = up_list.indexOf(fl) + 1;
+        return index;
+    }
+    else {
+        index = this.down_floors.size() + up_list.indexOf(fl);
+        return index;
+    }
 }
+Integer down(int fl, int flag){
+    ArrayList<Integer> down_list = new ArrayList<>(this.down_floors) ;
+    down_list.add(fl);
+    int index;
+    if (flag == 1) {
+        index = down_list.indexOf(fl) + 1;
+        return index;
+    }
+    else {
+        index = this.up_floors.size() + down_list.indexOf(fl);
+        return index;
+    }
+}
+Integer floor_distance(int fl){
+    if (fl == this.floor){
+        return 0;
+    }
+    if (fl > this.floor && this.up_floors.isEmpty()){
+        return 1;
+    }
+    if (fl < this.floor && this.down_floors.isEmpty()){
+        return 1;
+    }
+    if (fl > this.floor && this.direction == 1){
+        return up(fl, 1);
+    }
+    else if (fl > this.floor && this.direction == 0){
+        return up(fl, 0);
+    }
+    else if (fl < this.floor && this.direction == 0){
+        return down(fl, 1);
+    }
+    else {return down(fl, 0);}
+    }
+}
+
